ad5581348b9d725323d5f20c4ff7d0c8c2a68778
Yurii Popov <yurapopov522@gmail.com>
2024-09-13 17:51:35 +0300

    add typing

--- conference.py
+++ conference.py
@@ -1,33 +1,33 @@
 class Conference:
 
-    def __init__(self, name, number_of_participants, price, location, date_timestamp, maintainer):
-        self.__name = name
-        self.__number_of_participants = number_of_participants
-        self.__price = price
-        self.__location = location
+    def __init__(self, name: str, number_of_participants: int, price: int, location: str, date_timestamp: int, maintainer: str):
+        self.__name: str = name
+        self.__number_of_participants: int = number_of_participants
+        self.__price: int = price
+        self.__location: str = location
 
-        self.date_timestamp = date_timestamp
-        self.maintainer = maintainer
+        self.date_timestamp: int = date_timestamp
+        self.maintainer: str = maintainer
 
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__name
 
-    def get_number_of_participants(self):
+    def get_number_of_participants(self) -> int:
         return self.__number_of_participants
 
-    def get_price(self):
+    def get_price(self) -> int:
         return self.__price
 
-    def get_location(self):
+    def get_location(self) -> str:
         return self.__location
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Conference: {self.__name}, Number of Participants: {self.__number_of_participants}, Price: {self.__price}, Location: {self.__location}, Date: {self.date_timestamp}, Maintainer: {self.maintainer}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Conference: {self.__name}, Number of Participants: {self.__number_of_participants}, Price: {self.__price}, Location: {self.__location}, Date: {self.date_timestamp}, Maintainer: {self.maintainer}"
 
-    def __del__(self):
+    def __del__(self) -> None:
         print(f"{self.__name} is deleted")
         del self
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from conference import Conference
 
 
 def main():
-    first_conference = Conference("Python Conference", 100, 1000, "New York", "2021-10-10", "Python Software Foundation")
-    second_conference = Conference("Java Conference", 200, 2000, "San Francisco", "2021-10-20", "Java Community")
-    third_conference = Conference("JavaScript Conference", 300, 3000, "Los Angeles", "2021-10-30", "JavaScript Community")
+    first_conference: Conference = Conference("Python Conference", 100, 1000, "New York", "2021-10-10", "Python Software Foundation")
+    second_conference: Conference = Conference("Java Conference", 200, 2000, "San Francisco", "2021-10-20", "Java Community")
+    third_conference: Conference = Conference("JavaScript Conference", 300, 3000, "Los Angeles", "2021-10-30", "JavaScript Community")
     print(f"First Conference: {first_conference}\nSecond Conference: {second_conference}\nThird Conference: {third_conference}")
 
 
